6936b62d4fe0e8af03cf3639f2755cf9394d2eae
matheusmurray <matheus.murray@wolk.com.br>
2023-07-19 12:24:15 -0300

    feat(layout): gerar banners

--- resources/views/banners.blade.php
+++ resources/views/banners.blade.php
@@ -22,6 +22,60 @@
                     </a>
                 </div>
             </div>
+            <div class="h-5/6 my-20">
+                <label class="block mb-3 mt-3 text-large font-medium text-gray-900 dark:text-white">Nome do Profissional</label>
+                <input class="mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                <label class="block mb-3 mt-3 text-large font-medium text-gray-900 dark:text-white">Cargo</label>
+                <input class="mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+
+                <label class="block mb-3 mt-16 text-large font-medium text-gray-900 dark:text-white">1ª Tecnologia</label>
+                <select class="mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                    <option value="">React</option>
+                    <option value="">Vue</option>
+                    <option value="">Node</option>
+                    <option value="">Laravel</option>
+                    <option value="">PHP</option>
+                    <option value="">Ruby</option>
+                    <option value="">C#</option>
+                    <option value="">Svelte</option>
+                </select>
+                <label class="block mb-3 mt-6 text-large font-medium text-gray-900 dark:text-white">2ª Tecnologia</label>
+                <select class="mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                    <option value="">React</option>
+                    <option value="">Vue</option>
+                    <option value="">Node</option>
+                    <option value="">Laravel</option>
+                    <option value="">PHP</option>
+                    <option value="">Ruby</option>
+                    <option value="">C#</option>
+                    <option value="">Svelte</option>
+                </select>
+                <label class="block mb-3 mt-6 text-large font-medium text-gray-900 dark:text-white">3ª Tecnologia</label>
+                <select class="mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                    <option value="">React</option>
+                    <option value="">Vue</option>
+                    <option value="">Node</option>
+                    <option value="">Laravel</option>
+                    <option value="">PHP</option>
+                    <option value="">Ruby</option>
+                    <option value="">C#</option>
+                    <option value="">Svelte</option>
+                </select>
+                <label class="block mb-3 mt-6 text-large font-medium text-gray-900 dark:text-white">4ª Tecnologia</label>
+                <select class="mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                    <option value="">React</option>
+                    <option value="">Vue</option>
+                    <option value="">Node</option>
+                    <option value="">Laravel</option>
+                    <option value="">PHP</option>
+                    <option value="">Ruby</option>
+                    <option value="">C#</option>
+                    <option value="">Svelte</option>
+                </select>
+                <div class="flex justify-end mt-16">
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Gerar</button>
+                </div>
+            </div>
         </div>
     </div>
 </body>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,12 +1,15 @@
 <div id="side-navbar" class="min-h-screen bg-gray-200 w-1/5 shadow-2xl py-2">
     <div id="logo-brease" class="text-3xl p-3 text-gray-900 flex justify-center py-16">
-        Dev Banner Linkedin
+        <div class="w-7 mr-2 flex justify-center">
+            <img src="{{URL::asset('/image/box-black.svg')}}" alt="profile Pic" height="30" width="30">
+        </div>
+        devbannerlinkedin
     </div>
 
     <div class="p-3 pt-12 text-gray-900 mx-4 text-blue-500 hover:text-blue-800">
         <a class="flex" href="{{ url('/banners') }}">
             <div class="w-5 mr-3 flex justify-center">
-                <img src="{{URL::asset('/image/box.svg')}}" alt="profile Pic" height="18" width="18">
+                <img src="{{URL::asset('/image/box-black.svg')}}" alt="profile Pic" height="18" width="18">
             </div>
             <p>Gerar Banner</p>
         </a>
@@ -14,7 +17,7 @@
     <div class="p-3 text-gray-900 mx-4 text-blue-500 hover:text-blue-800">
         <a class="flex" href="{{ url('/banners') }}">
             <div class="w-5 mr-3 flex justify-center">
-                <img src="{{URL::asset('/image/box.svg')}}" alt="profile Pic" height="18" width="18">
+                <img src="{{URL::asset('/image/box-black.svg')}}" alt="profile Pic" height="18" width="18">
             </div>
             <p>Gerar Sobre</p>
         </a>
@@ -22,7 +25,7 @@
     <div class="p-3 text-gray-900 mx-4 text-blue-500 hover:text-blue-800">
         <a class="flex" href="{{ url('/banners') }}">
             <div class="w-5 mr-3 flex justify-center">
-                <img src="{{URL::asset('/image/box.svg')}}" alt="profile Pic" height="18" width="18">
+                <img src="{{URL::asset('/image/box-black.svg')}}" alt="profile Pic" height="18" width="18">
             </div>
             <p>Gerar Experiências</p>
         </a>
@@ -30,7 +33,7 @@
     <div class="p-3 text-gray-900 mx-4 text-blue-500 hover:text-blue-800">
         <a class="flex" href="{{ url('/banners') }}">
             <div class="w-5 mr-3 flex justify-center">
-                <img src="{{URL::asset('/image/box.svg')}}" alt="profile Pic" height="18" width="18">
+                <img src="{{URL::asset('/image/box-black.svg')}}" alt="profile Pic" height="18" width="18">
             </div>
             <p>Gerar Educação</p>
         </a>
@@ -38,7 +41,7 @@
     <div class="p-3 text-gray-900 mx-4 text-blue-500 hover:text-blue-800">
         <a class="flex" href="{{ url('/banners') }}">
             <div class="w-5 mr-3 flex justify-center">
-                <img src="{{URL::asset('/image/box.svg')}}" alt="profile Pic" height="18" width="18">
+                <img src="{{URL::asset('/image/box-black.svg')}}" alt="profile Pic" height="18" width="18">
             </div>
             <p>Gerar Currículo</p>
         </a>
@@ -46,7 +49,7 @@
     <div class="p-3 text-gray-900 mx-4 text-blue-500 hover:text-blue-800">
         <a class="flex" href="{{ url('/banners') }}">
             <div class="w-5 mr-3 flex justify-center">
-                <img src="{{URL::asset('/image/box.svg')}}" alt="profile Pic" height="18" width="18">
+                <img src="{{URL::asset('/image/box-black.svg')}}" alt="profile Pic" height="18" width="18">
             </div>
             <p>Converter PDF Inglês</p>
         </a>
