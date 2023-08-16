fd1043607cebf3f9fca7fa835e973d7c951e9c50
matheusmurray <matheus.murray@wolk.com.br>
2023-08-16 17:25:02 -0300

    feat: banner estatico

--- app/Domain/Banners/BannerGenerator.php
+++ app/Domain/Banners/BannerGenerator.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 class BannerGenerator
 {
     protected $banner;
-    protected $imageWidth = 120;
+    protected $imageWidth = 180;
     protected $spacing = 20;
     protected $fontSize = 18;
     protected $textColor;
@@ -24,34 +24,50 @@ class BannerGenerator
     public function generate()
     {
         $this->positionImages();
-        $this->addText('matheusmurraydev@gmail.com', 20, 20);
-        $this->addText('SOFTWARE ENGINEER', 20, 38 + $this->fontSize);
+        
+        $textMargin = 35; // Adjust this value for vertical spacing
+        $positionY = 20 + $textMargin; // Add top margin of 20px to position Y
+        $lineSpacing = 10; // Adjust this value to control the spacing between lines
+        $rightMargin = 35; // Adjust this value to increase the right margin
+        
+        $emailY = $positionY + $this->fontSize + $lineSpacing; // Adjust spacing between lines
+    
+        // Align text to the right with the specified right margin
+        $this->addText('matheusmurraydev@gmail.com', 1585 - $rightMargin, $emailY);
+        $this->addText('SOFTWARE ENGINEER', 1585 - $rightMargin, $positionY);
+        
         $this->saveBanner();
+        
         return $this->banner;
     }
-
+    
     protected function positionImages()
     {
         $pngFiles = ['react.png', 'node.png', 'firebase.png', 'gcp.png'];
-        $xPosition = $this->banner->width() - $this->spacing;
-
+        $xPosition = $this->banner->width() - $this->spacing - $this->imageWidth - 10; // Adjusted X position
+    
         foreach ($pngFiles as $pngFile) {
             $png = Image::make(public_path('image/' . $pngFile));
             $png->resize($this->imageWidth, null, function ($constraint) {
                 $constraint->aspectRatio();
             });
-
-            $yPosition = $this->banner->height() - $png->height() - 20;
-            $this->banner->insert($png, 'top-left', $xPosition - $png->width(), $yPosition);
-
-            $xPosition -= $png->width() + $this->spacing;
+    
+            $yPosition = $this->banner->height() - $png->height() - 30;
+            $this->banner->insert($png, 'top-left', $xPosition, $yPosition);
+    
+            $xPosition -= $this->imageWidth + $this->spacing;
         }
     }
 
     protected function addText($text, $x, $y)
     {
         $fontPath = public_path('fonts/Roboto-Regular.ttf');
-        imagettftext($this->banner->getCore(), $this->fontSize, 0, $x, $y, $this->textColor, $fontPath, $text);
+        $textWidth = imagettfbbox($this->fontSize, 0, $fontPath, $text);
+        $textWidth = $textWidth[2] - $textWidth[0]; // Calculate text width
+    
+        $adjustedX = $x - $textWidth; // Adjust X position based on text width
+    
+        imagettftext($this->banner->getCore(), $this->fontSize, 0, $adjustedX, $y, $this->textColor, $fontPath, $text);
     }
 
     protected function saveBanner()
